8fd544df613a08cc1d20cf6af81fd60aa195f4b1
terlek <terlek@DESKTOP-RL37VNN>
2022-04-10 21:21:33 +0100

    Added User Controller with corresponding Entity Framework Views

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using EAD_CA_PROJECT_INVEST.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<EAD_CA_PROJECT_INVESTContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("EAD_CA_PROJECT_INVESTContext")));
 
 var app = builder.Build();
 
